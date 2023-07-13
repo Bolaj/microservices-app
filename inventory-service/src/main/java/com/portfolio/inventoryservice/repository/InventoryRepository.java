6de43e7b7a015c66d313f78ad0522c99fab73c22
TimiBolaji <mattybolaji@gmail.com>
2023-07-14 00:59:13 +0100

    --

---
+++ inventory-service/src/main/java/com/portfolio/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.inventoryservice.repository;
+
+import com.portfolio.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode();
+}
